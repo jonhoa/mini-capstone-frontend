1ac0f48cf129762477cc071491d2eba66b4d3669
Jonathan <jonitoadriano@gmail.com>
2023-01-18 14:23:04 -0600

    move code into jsx files

---
+++ src/Content.jsx
@@ -0,0 +1,11 @@
+import { ProductsNew } from "./ProductsNew";
+import { ProductsIndex } from "./ProductsIndex";
+
+export function Content() {
+  return (
+    <div>
+      <ProductsNew />
+      <ProductsIndex />
+    </div>
+  );
+}
