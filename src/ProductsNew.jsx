c84abd30ff24c66515b729b2ccf8145fb9823701
Jonathan <jonitoadriano@gmail.com>
2023-01-19 12:45:24 -0600

    create products new

--- src/ProductsNew.jsx
+++ src/ProductsNew.jsx
@@ -1,2 +1,15 @@
 
-export function ProductsNew() { }
+export function ProductsNew() { 
+  return (
+    <div>
+      <h1>Products New</h1>
+      <form>
+        <p>Name <input type ="text" name ="name"></input></p>
+        <p>Price <input type ="text" name ="name"></input></p>
+        <p>Description <input type ="text" name ="description"></input></p>
+        <button type="submit">Submit</button> 
+      </form>
+      <hr/>
+    </div>
+  );
+}
